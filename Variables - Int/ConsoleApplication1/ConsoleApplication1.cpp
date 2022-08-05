5a351242140709f8d2d2fcc59a58d82b20d7d6cc
Shailesh Khune <khuneshailesh73@gmail.com>
2022-08-05 20:15:59 +0100

    added code for integers

---
+++ Variables - Int/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myNum = 42069;
+
+    cout << myNum << endl; // int is used to store the whole number such as 123 to -123!
+
+    return 0;
+
+}
+
